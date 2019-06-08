952f0afdac4a9d7c32403feaa9a44716016d4df3
ishizukayusuke <ishizukayusuke@team-lab.com>
2019-06-08 16:11:09 +0900

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>hello header!</p>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['user'])
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <Header />
+    <logo />
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Logo,
+    Header
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
